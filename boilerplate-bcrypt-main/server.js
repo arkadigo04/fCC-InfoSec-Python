80ca6ce22eaa86323fb9a60de1ec6054231ffa7b
arkaitz <arkaitz@vbox.GNC.DOM>
2026-03-27 13:57:13 +0100

    feat(auth): implementar hashing y comparacion asincrona de contraseñas
    - Añadido bcrypt.hash() para generar hashes seguros sin bloquear el Event Loop. - Añadido bcrypt.compare() para la validación de credenciales.

--- boilerplate-bcrypt-main/server.js
+++ boilerplate-bcrypt-main/server.js
@@ -8,11 +8,22 @@ const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 
+const bcrypt = require('bcrypt');
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
-const bcrypt = require('bcrypt');
-
+//START_ASYNC -do not remove notes, place code between correct pair of notes.
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+  // 1. Imprimimos el hash generado para verlo
+  console.log(hash); 
+  
+  // 2. DENTRO de la función, comparamos la contraseña original con el hash recién creado
+  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+    // 3. Imprimimos el resultado de la comparación (debería ser true)
+    console.log(res); 
+  });
+});
+//END_ASYNC
 //END_ASYNC
 
 //START_SYNC
